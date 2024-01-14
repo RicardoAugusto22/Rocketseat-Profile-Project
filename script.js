0cba4ce5e81993ff6f839383b91839aa1f053cc7
Ricardo Augusto <ricardo.a.g.a@gmail.com>
2024-01-14 16:47:20 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+  
+  // pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  // substituir a image
+  if(html.classList.contains('light')) {
+    // se estiver no light mode, adicionar a imagem light
+    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute(
+      "alt",
+      "Foto de Mayk Brito sorrindo, usando óculos escuros e camisa preta, barba e fundo roxo e azul."
+    )
+  } else {
+    // se estiver no dark mode, manter a imagem normal
+    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute(
+      "alt",
+      "Foto de Mayk Brito sorrindo, usando óculos e camisa preta, barba e fundo amarelo."
+    )
+  }
+}
